fab245a4d27e6d1b1911f63c7132931034c09b7d
José Espinoza <espinozasalas.jose@gmail.com>
2019-12-26 20:37:20 -0300

    Commented Rut & RutFormat code

--- ChileanRut/Rut.cs
+++ ChileanRut/Rut.cs
@@ -4,6 +4,9 @@ using System.Text.RegularExpressions;
 
 namespace MrCoto.ChileanRut
 {
+    /// <summary>
+    /// Rut Chileno.
+    /// </summary>
     public class Rut : IComparable<Rut>
     {
         private static readonly string RGX_NUMBER = @"^([1-9]\d?(\.?\d{3}){0,2}|[1-9]\d{0,2}(\.?\d{3})?)$";
@@ -20,6 +23,11 @@ namespace MrCoto.ChileanRut
         private int _number;
         private string _dv;
 
+        /// <summary>
+        /// Constructor principal.
+        /// </summary>
+        /// <param name="number">Parte númerica de un rut (de 123-k, la parte númerica es "123").</param>
+        /// <param name="dv">Dígito verificador de un rut.</param>
         public Rut(string number, string dv)
         {   
             if (!Regex.Match(number, RGX_NUMBER).Success) throw new ArgumentException("Formato Inválido");
@@ -28,6 +36,19 @@ namespace MrCoto.ChileanRut
             _dv = dv.ToLower();
         }
 
+        /// <summary>
+        /// Obtiene un Rut dada la parte numérica y su dv.
+        /// 
+        /// Ejemplo formatos aceptados:
+        /// 
+        /// - 12345678-k
+        /// - 12345678k
+        /// - 12.345.678-k
+        /// - 12.345.678k
+        /// - 12.345.678K.
+        /// </summary>
+        /// <param name="rut">Rut con su parte númerica y dv.</param>
+        /// <returns>Instancia de un Rut</returns>
         public static Rut Parse(string rut)
         {
             if (string.IsNullOrWhiteSpace(rut) || rut == ZERO) return new Rut("", "");
@@ -37,8 +58,18 @@ namespace MrCoto.ChileanRut
             return new Rut(number, dv);
         }
 
+        /// <summary>
+        /// Obtiene un Rut dada la parte numérica.
+        /// </summary>
+        /// <param name="number">Parte numérica del RUT (Ej si es 123-k, sería 123).</param>
+        /// <returns>Instancia de un Rut</returns>
         public static Rut Parse(int number) => new Rut(number.ToString(), CalcDv(number));
 
+        /// <summary>
+        /// Calcula el digito verificador de un Rut (data una parte numérica).
+        /// </summary>
+        /// <param name="number">Parte númerica del rut.</param>
+        /// <returns>Dígito verificador.</returns>
         public static string CalcDv(int number)
         {
             int[] series = {2, 3, 4, 5, 6, 7};
@@ -59,12 +90,27 @@ namespace MrCoto.ChileanRut
             _ => result.ToString()
         };
 
+        /// <summary>
+        /// Genera un Rut de forma aleatoria.
+        /// </summary>
+        /// <param name="min">Valor mínimo que podría tener la parte numérica del Rut.</param>
+        /// <param name="max">Valor máximo que podría tener la parte numérica del Rut.</param>
+        /// <param name="seed">Indica si el RUT se generará mediante un seed dado. null si no se debe aplicar.</param>
+        /// <returns>Rut aleatorio.</returns>
         public static Rut Random(int min = MIN_RANGE, int max = MAX_RANGE, int? seed = null)
         {
             var random = seed == null ? new Random() : new Random((int) seed);
             return Rut.Parse(random.Next(min, max));
         }
 
+        /// <summary>
+        /// Genera una lista de Ruts de forma aleatoria
+        /// </summary>
+        /// <param name="n">Tamaño de la lista.</param>
+        /// <param name="min">Valor mínimo que podría tener la parte numérica del Rut.</param>
+        /// <param name="max">Valor máximo que podría tener la parte numérica del Rut.</param>
+        /// <param name="seed">Indica si el RUT se generará mediante un seed dado. null si no se debe aplicar.</param>
+        /// <returns>Lista de Ruts aleatorios.</returns>
         public static List<Rut> Randoms(int n = 1, int min = MIN_RANGE, int max = MAX_RANGE, int? seed = null)
         {
             var random = seed == null ? new Random() : new Random((int) seed);
@@ -73,6 +119,14 @@ namespace MrCoto.ChileanRut
             return rutList;
         }
 
+        /// <summary>
+        /// Genera una lista de Ruts únicos de forma aleatoria
+        /// </summary>
+        /// <param name="n">Tamaño de la lista.</param>
+        /// <param name="min">Valor mínimo que podría tener la parte numérica del Rut.</param>
+        /// <param name="max">Valor máximo que podría tener la parte numérica del Rut.</param>
+        /// <param name="seed">Indica si el RUT se generará mediante un seed dado. null si no se debe aplicar.</param>
+        /// <returns>Lista única de Ruts aleatorios.</returns>
         public static List<Rut> Uniques(int n = 1, int min = MIN_RANGE, int max = MAX_RANGE, int? seed = null)
         {
             var random = seed == null ? new Random() : new Random((int) seed);
@@ -81,8 +135,21 @@ namespace MrCoto.ChileanRut
             return rutUniqueList;
         }
 
+        /// <summary>
+        /// Valida un Rut.
+        /// </summary>
+        /// <returns>Verdadero si el RUT es válido.</returns>
         public bool IsValid() => CalcDv(_number) == _dv;
         
+        /// <summary>
+        /// Formatea el RUT.
+        /// 
+        /// - [RUTFORMAT.FULL] -> RUT con puntos y guión (12.345.678-k)
+        /// - [RUTFORMAT.ONLY_DASH] -> RUT sin puntos y con guión (12345678-k)
+        /// - [RUTFORMAT.ESCAPED] -> RUT sin puntos y guión (12345678k)
+        /// </summary>
+        /// <param name="format">Formato del RUT</param>
+        /// <returns>String del Rut formateado.</returns>
         public string Format(RutFormat format = RutFormat.FULL) => format switch
         {
             RutFormat.FULL => $"{_number:n0}-{_dv}".Replace(",", "."),
@@ -91,8 +158,18 @@ namespace MrCoto.ChileanRut
             _ => this.Format() 
         };
 
+        /// <summary>
+        /// Compara esta instancia de Rut con otra,
+        /// </summary>
+        /// <param name="other">La otra instancia de Rut.</param>
+        /// <returns>Valor de comparación.</returns>
         public int CompareTo(Rut other) => _number.CompareTo(other._number);
 
+        /// <summary>
+        /// Igualdad estructural.
+        /// </summary>
+        /// <param name="obj">Otra Instancia (o null).</param>
+        /// <returns>Verdadero si son un Rut es igual a otro (estructuralmente).</returns>
         #nullable enable
         public override bool Equals(object? obj)
         {
@@ -105,21 +182,70 @@ namespace MrCoto.ChileanRut
         }
         #nullable disable
 
+        /// <summary>
+        /// Hash Code.
+        /// </summary>
+        /// <returns>Valor hash.</returns>
         public override int GetHashCode() => 31 * _number + _dv.GetHashCode();
 
+        /// <summary>
+        /// Representación de un Rut.
+        /// </summary>
+        /// <returns>Representación en string de un Rut.</returns>
         public override string ToString() => $"Rut({_number}, {_dv})";
 
+        /// <summary>
+        /// Destructuración de un Rut.
+        /// </summary>
+        /// <param name="number">Parte numérica del Rut.</param>
+        /// <param name="dv">Dígito verificador del Rut.</param>
         public void Deconstruct(out int number, out string dv)
         {
             number = _number;
             dv = _dv;
         }
 
+        /// <summary>
+        /// Definición de 'Menor que'.
+        /// </summary>
+        /// <param name="x">Operando Izquierdo.</param>
+        /// <param name="y">Operando Derecho.</param>
+        /// <returns>Verdadero Si x es menor que y.</returns>
         public static bool operator <(Rut x, Rut y) => x.CompareTo(y)  < 0;
+        /// <summary>
+        /// Definición de 'Menor o igual que'.
+        /// </summary>
+        /// <param name="x">Operando Izquierdo.</param>
+        /// <param name="y">Operando Derecho.</param>
+        /// <returns>Verdadero Si x es menor o igual que y.</returns>
         public static bool operator <=(Rut x, Rut y) => x.CompareTo(y)  <= 0;
+        /// <summary>
+        /// Definición de 'Mayor que'.
+        /// </summary>
+        /// <param name="x">Operando Izquierdo.</param>
+        /// <param name="y">Operando Derecho.</param>
+        /// <returns>Verdadero Si x es mayor que y.</returns>
         public static bool operator >(Rut x, Rut y) => x.CompareTo(y)  > 0;
+        /// <summary>
+        /// Definición de 'Mayor o igual que'.
+        /// </summary>
+        /// <param name="x">Operando Izquierdo.</param>
+        /// <param name="y">Operando Derecho.</param>
+        /// <returns>Verdadero Si x es mayor o igual que y.</returns>
         public static bool operator >=(Rut x, Rut y) => x.CompareTo(y)  >= 0;
+        /// <summary>
+        /// Definición de 'Igual que'.
+        /// </summary>
+        /// <param name="x">Operando Izquierdo.</param>
+        /// <param name="y">Operando Derecho.</param>
+        /// <returns>Verdadero Si x es igual que y.</returns>
         public static bool operator ==(Rut x, Rut y) => x.Equals(y);
+        /// <summary>
+        /// Definición de 'Distinto de'.
+        /// </summary>
+        /// <param name="x">Operando Izquierdo.</param>
+        /// <param name="y">Operando Derecho.</param>
+        /// <returns>Verdadero Si x es distinto de y.</returns>
         public static bool operator !=(Rut x, Rut y) => !x.Equals(y);
 
     }
--- ChileanRut/RutFormat.cs
+++ ChileanRut/RutFormat.cs
@@ -1,9 +1,26 @@
 namespace MrCoto.ChileanRut
 {
+    /// <summary>
+    /// Formatos de un Rut.
+    /// 
+    /// - [FULL]: Formato con puntos y guión (12.345.678-9)
+    /// - [ONLY_DASH]: Formato solo sin puntos y con guión (12345678-9)
+    /// - [ESCAPED]: Formato sin puntos ni guión (123456789)
+    /// </summary>
     public enum RutFormat
     {
+        
+        /// <summary>
+        /// [FULL]: Formato con puntos y guión (12.345.678-9)
+        /// </summary>
         FULL,
+        /// <summary>
+        /// [ONLY_DASH]: Formato solo sin puntos y con guión (12345678-9)
+        /// </summary>
         ONLY_DASH,
+        /// <summary>
+        /// [ESCAPED]: Formato sin puntos ni guión (123456789)
+        /// </summary>
         ESCAPED
     }
 }
